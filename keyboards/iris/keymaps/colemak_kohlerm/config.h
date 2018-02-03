0b0206974695973c26829daf60f49b16006af9b9
kohlerm <markus.kohler@gmail.com>
2018-02-03 18:28:45 +0100

    iris avoid sticking mods

--- keyboards/iris/keymaps/colemak_kohlerm/config.h
+++ keyboards/iris/keymaps/colemak_kohlerm/config.h
@@ -26,7 +26,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define USE_I2C
 
 /* Select hand configuration */
-
+#define PERMISSIVE_HOLD
+#define PREVENT_STUCK_MODIFIERS
 #define MASTER_LEFT
 // #define MASTER_RIGHT
 // #define EE_HANDS
